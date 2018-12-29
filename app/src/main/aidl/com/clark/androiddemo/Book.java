22b53793c4ab36a290df3cd3a2555462dc39c240
zhangdi <1017397593@qq.com>
2018-12-29 18:54:20 +0800

    Initial commit

---
+++ app/src/main/aidl/com/clark/androiddemo/Book.java
@@ -0,0 +1,66 @@
+package com.clark.androiddemo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * create by SuperClark 2018/12/28
+ */
+public class Book implements Parcelable {
+
+    private int bookId;
+    private String bookName;
+
+    public Book(int bookId, String bookName) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+    }
+
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(bookId);
+        dest.writeString(bookName);
+    }
+
+
+    private Book(Parcel in) {
+        bookId = in.readInt();
+        bookName = in.readString();
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+
+}
